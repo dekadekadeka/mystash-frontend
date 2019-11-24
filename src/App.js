2ae2ac1163479214ee89d6724a92b0142d3a6093
github username <dekadekadeka@gmail.com>
2019-11-23 20:33:42 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import List from './pages/List'
 import Stash from './pages/Stash'
 import Patterns from './pages/Patterns'
 import SinglePattern from './pages/SinglePattern'
@@ -9,22 +10,24 @@ import Login from './pages/Login'
 import Error from './pages/Error'
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      {localStorage.token && this.props.user_id ? null :
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/stash" component={Stash} />
-        <Route exact path="/patterns" component={Patterns} />
-        <Route exact path="/patterns/:number" component={SinglePattern} />
-        <Route exact path="/login" component={Login} />
-        <Route component={Error} />
-      </Switch>
-      }
-    </div>
-  );
+export class App extends Component {
+  render(){
+    return (
+      <div>
+        <Navbar />
+        {localStorage.token && this.props.user_id ? null :
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/stash" component={Stash} />
+          <Route exact path="/list" component={List} />
+          <Route exact path="/patterns" component={Patterns} />
+          <Route exact path="/patterns/:number" component={SinglePattern} />
+          <Route exact path="/login" component={Login} />
+          <Route component={Error} />
+        </Switch>
+        }
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            I am a navbar
+        <div className="navbar">
+            {/* {this.props.currentUser.username ? */}
+            {/* <Link to="/stash"><h1>myStash</h1></Link> : */}
+            <Link to="/login"><h1>myStash</h1></Link>
+            {/* } */}
         </div>
     );
 }
