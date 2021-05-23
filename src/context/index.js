5bbfac463ce219d7be71026e716cc61b1301dba3
Deka Ambia <dekadekadeka@gmail.com>
2021-05-23 17:40:35 -0400

    remove all old files

--- src/context/index.js
+++
@@ -1,4 +0,0 @@
-import { loginUser, logout } from './UserActions';
-import { UserProvider, useAuthDispatch, useAuthState } from './UserContext';
-
-export { UserProvider, useAuthState, useAuthDispatch, loginUser, logout };
